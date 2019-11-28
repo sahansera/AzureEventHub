7650edaffc8a7f5104acedb2e3157cab6b48eba6
Sahan Serasinghe <sahan.serasinghe@readify.net>
2019-11-28 14:46:10 +1030

    Add project files.

---
+++ EventHubReceiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Threading.Tasks;
+
+namespace EventHubReceiver
+{
+    class Program
+    {
+        private const string EventHubConnectionString = "";
+        private const string EventHubName = "";
+        private const string StorageContainerName = "";
+        private const string StorageAccountName = "";
+
+        private const string StorageAccountKey = "";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        // Receiver - This is a special case where we need to make
+        // use of EventProcessor
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiving Messages");
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ EventHubTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace EventHubTest
+{
+
+    using Microsoft.Azure.EventHubs;
+    using Newtonsoft.Json;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "";
+        private const string EventHubName = "";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sending Data...");
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        // Sender
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but this simple scenario
+            // uses the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(100);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Uses the event hub client to send 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var tmp = new { message = $"This is message {i}"};
+                    var message = JsonConvert.SerializeObject(tmp);
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+
+    }
+}
